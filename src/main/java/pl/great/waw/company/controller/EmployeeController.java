6ee1f69f19ea843cba7f40ef9f64793e11ecead4
pszczepkowski <p.szczepkowski87@gmail.com>
2023-02-02 20:24:20 +0100

    fix dev

--- src/main/java/pl/great/waw/company/controller/EmployeeController.java
+++ src/main/java/pl/great/waw/company/controller/EmployeeController.java
@@ -2,26 +2,18 @@ package pl.great.waw.company.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.*;
-import pl.great.waw.company.Json.JsonParsing;
 import pl.great.waw.company.exceptions.PeselAlreadyExistException;
 import pl.great.waw.company.exceptions.PeselNotFoundException;
-import pl.great.waw.company.model.Employee;
 import pl.great.waw.company.service.EmployeeDto;
 import pl.great.waw.company.service.EmployeeServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static springfox.documentation.builders.PathSelectors.any;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 
     private final EmployeeServiceImpl employeeService;
-
-    private List<Employee> employeeList = new ArrayList<>();
-
 
     public EmployeeController( EmployeeServiceImpl employeeService) {
         this.employeeService = employeeService;
@@ -43,34 +35,16 @@ public class EmployeeController {
         }
         employeeService.create(employeeDto);
 
-        Employee employee = new Employee(employeeDto.getPesel(), employeeDto.getFirstName(), employeeDto.getLastName(), employeeDto.getSalary());
-        employeeList.add(employee);
-
         return employeeDto;
     }
 
     @DeleteMapping(value = "{pesel}")
     public boolean delete(@PathVariable String pesel) throws PeselNotFoundException {
-        boolean isDeleted = employeeService.delete(pesel);
-
-        if(isDeleted){
-            employeeList.removeIf(employee -> employee.getPesel().equals(pesel));
-        }return isDeleted;
+        return employeeService.delete(pesel);
     }
 
     @PutMapping(value = "{pesel}")
     public EmployeeDto update(@PathVariable String pesel, @RequestBody EmployeeDto employeeDto) throws PeselNotFoundException {
-        EmployeeDto updatedEmployee= employeeService.update(pesel, employeeDto);
-return null;
-//        if(updatedEmployee != null){ employeeList.stream().filter(employee -> employee.getPesel().equals(pesel))
-//                .findFirst()
-//                .ifPresent(employee -> {
-//        employee.getUpdated(employeeDto.getFirstName());
-//        employee.getUpdated(employeeDto.getLastName());
-//        employee.getUpdated(employeeDto.getSalary());
-//                });
- //       }return updatedEmployee;
+        return employeeService.update(pesel, employeeDto);
     }
-
-
 }
