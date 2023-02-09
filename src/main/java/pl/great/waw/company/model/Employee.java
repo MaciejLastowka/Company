03ba2fa2fff2a85340214e3d4d0f3634bcd6f813
Maciej Łastówka <maciek240395@gmail.com>
2023-02-09 16:41:34 +0000

    Merged in COMP-12_Maciek (pull request #11)
    COMP-12 Maciek
    
    * fixed parsing json test
    
    * fixed parsing json test
    
    * fixed parsing json test
    
    
    Approved-by: Paweł Szczepkowski

--- src/main/java/pl/great/waw/company/model/Employee.java
+++ src/main/java/pl/great/waw/company/model/Employee.java
@@ -2,18 +2,21 @@ package pl.great.waw.company.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Employee {
 
-    private  String pesel;
-    private  String firstName;
-    private  String lastName;
-    private  BigDecimal price;
-    private  LocalDateTime created;
-    private  LocalDateTime updated;
+    private String pesel;
+    private String firstName;
+    private String lastName;
+    private BigDecimal price;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
     public Employee() {
+
     }
+
 
     public Employee(String pesel, String firstName, String lastName, BigDecimal price, LocalDateTime created, LocalDateTime updated) {
         this.pesel = pesel;
@@ -59,4 +62,16 @@ public class Employee {
         return updated;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(pesel, employee.pesel) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(price, employee.price) && Objects.equals(created, employee.created) && Objects.equals(updated, employee.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pesel, firstName, lastName, price, created, updated);
+    }
 }
